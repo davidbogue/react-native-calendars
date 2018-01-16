29d7eba71f5d23477223989ea8b63f0c30af7503
Matt Nicolls <mnicolls@technologypartners.net>
2018-01-16 16:58:05 -0600

    changed render logic to only render an reservation container if the content returned from renterItem or renderEmptyDate returns a non null value

--- src/agenda/reservation-list/reservation.js
+++ src/agenda/reservation-list/reservation.js
@@ -63,14 +63,12 @@ class ReservationListItem extends Component {
     // todo: [mattnicolls 32018-01-16] this is where I've forked the code
 	// the point is to hide the container if the child content is null
 
-	const comp = content !== null ? <View style={this.styles.container}>
+	return = content !== null ? <View style={this.styles.container}>
 	  	{this.renderDate(date, reservation)}
   		<View style={{flex:1}}>
 	  		{content}
   		</View>
-	</View> ? null
-
-    return (comp);
+	</View> ? null;
   }
 }
 
